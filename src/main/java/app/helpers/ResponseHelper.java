79ae8243a437e1e46e41aef749b3dea0a46cf0e9
Marco Molica <marco.molica@datlas.it>
2023-07-31 21:11:22 +0200

    Add cors

--- src/main/java/app/helpers/ResponseHelper.java
+++ src/main/java/app/helpers/ResponseHelper.java
@@ -9,32 +9,39 @@ import java.io.PrintWriter;
 
 public class ResponseHelper {
 
-    public static void ReturnErrorStatus(HttpServletResponse response, int httpStatus, String errorMessage) throws IOException {
-        var     error = new ErrorMessage(errorMessage);
-        response.setContentType("text/json");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setStatus(httpStatus);
-        PrintWriter out = response.getWriter();
-        out.println(JsonHelper.ToJson(error));
-    }
+  public static void ReturnErrorStatus(HttpServletResponse response, int httpStatus, String errorMessage)
+      throws IOException {
+    var error = new ErrorMessage(errorMessage);
+    response.setContentType("text/json");
+    enableCors(response);
+    response.setStatus(httpStatus);
+    PrintWriter out = response.getWriter();
+    out.println(JsonHelper.ToJson(error));
+  }
 
-    public static <T> void ReturnOk(HttpServletResponse response, T valueResponse) throws IOException {
-        response.setContentType("text/json");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setStatus(200);
-        PrintWriter out = response.getWriter();
-        var resp = JsonHelper.ToJson(valueResponse);
-        out.println(resp);
-    }
+  public static <T> void ReturnOk(HttpServletResponse response, T valueResponse) throws IOException {
+    response.setContentType("text/json");
+    enableCors(response);
+    response.setStatus(200);
+    PrintWriter out = response.getWriter();
+    var resp = JsonHelper.ToJson(valueResponse);
+    out.println(resp);
+  }
 
-    public static void ReturnOk(HttpServletResponse response) throws IOException {
-        response.setContentType("text/json");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setStatus(200);
-        PrintWriter out = response.getWriter();
-        out.println("");
-    }
+  public static void ReturnOk(HttpServletResponse response) throws IOException {
+    response.setContentType("text/json");
+    enableCors(response);
+    response.setStatus(200);
+    PrintWriter out = response.getWriter();
+    out.println("");
+  }
+
+  private static void enableCors(HttpServletResponse response) {
+    response.addHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    response.addHeader("Access-Control-Allow-Headers", "Authorization");
+    response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+    response.addHeader("Access-Control-Allow-Credentials", "true");
+  }
 }
+
